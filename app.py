24a265fdd8d2522f78c825c8736dcc588ae9514c
M-474 <mr6699474@gmail.com>
2025-02-22 14:46:17 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import keras
+from tensorflow.python import pywrap_tensorflow as _pywrap_tensorflow
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+#from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+
+app = Flask(__name__)
+model = ResNet50()
+
+@app.route('/', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile= request.files['imagefile']
+    image_path = r"C:\Users\tahab\OneDrive\Documents\ITAI Training Materials\ECU-ITAI-4-02\image-classifier-main\images" + imagefile.filename
+    imagefile.save(image_path)
+
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+
+    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
+
+
+    return render_template('index.html', prediction=classification)
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
